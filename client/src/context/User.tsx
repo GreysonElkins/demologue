75a96911b9a642b6ccdc8ccec1b7a35975f56be6
Greyson Elkins <greysonelkins@gmail.com>
2021-11-27 21:28:51 -0700

    Sync new users with postgres

--- client/src/context/User.tsx
+++ client/src/context/User.tsx
@@ -1,6 +1,9 @@
 import { createContext, useContext, useState, useEffect, useCallback } from 'react'
 import firebase from 'firebase/compat/app'
 import auth from 'scripts/auth'
+import { ParsedFirebaseUser } from 'types/firebase'
+import { getUser } from 'scripts/api/demologue/query/user'
+import UserMutations from 'scripts/api/demologue/mutation/user'
 
 type UserContextValue = {
   loading: boolean
@@ -8,11 +11,13 @@ type UserContextValue = {
   signedIn: boolean
 }
 
-import { ParsedFirebaseUser } from 'types/firebase'
 const UserContext = createContext({} as UserContextValue)
 
 export const UserProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<ParsedFirebaseUser | null>(null)
+  const { status, data, isFetching } = getUser(user?.uid)
+  const { createUser } = UserMutations()
+  
   const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
@@ -22,6 +27,18 @@ export const UserProvider: React.FC = ({ children }) => {
       setLoading(false)
     })
   }, [])
+
+  useEffect(() => {
+    if (status === 'success' && !isFetching && !data && !!user) {
+      createUser(user)
+    } 
+    // else {
+    //   checkForUpdates()
+    // maybe firebase should JUST be about JWT auth and share it's first set of data
+    // updates afterwards will be made in UI and if needed, update firebase as well
+    // but column `isVerified` can probably be ignored
+    // }
+  }, [status, data, isFetching])
 
   const parseUser = useCallback((data: firebase.User) => {
     const { displayName, email, emailVerified, uid, photoURL } = data
