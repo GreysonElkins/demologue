d15d8691012de112cf5c82da6de55e1197241c5c
Greyson Elkins <greysonelkins@gmail.com>
2021-11-27 19:22:09 -0700

    Move query provider

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,12 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { UserProvider } from 'context/User'
+
 import Header from 'components/Header'
-import { UserProvider } from "context/User"
 import './App.scss'
 
+const queryClient = new QueryClient()
+
 const App: React.FC = () => (
-  <UserProvider>
-    <Header />
-    <main></main>
-  </UserProvider>
+  <QueryClientProvider client={queryClient}>
+    {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    <UserProvider>
+      <Header />
+      <main></main>
+    </UserProvider>
+  </QueryClientProvider>
 )
 
 export default App
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import App from 'App'
 
 import 'style/index.scss'
 
-const queryClient = new QueryClient()
-
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
       <App />
-    </QueryClientProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 )
