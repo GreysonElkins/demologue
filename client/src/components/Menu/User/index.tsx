b37a1e93e952a149b132fc300366600f4c2b221e
Greyson Elkins <greysonelkins@gmail.com>
2021-11-28 12:28:11 -0700

    Start user menu

--- client/src/components/Menu/User/index.tsx
+++ client/src/components/Menu/User/index.tsx
@@ -1,15 +1,26 @@
 import { useUser } from 'context/User'
+
 import DropdownCta from 'style/button/DropdownCta'
+import Avatar from 'style/button/Avatar'
 
 import './UserMenu.scss'
 
 const UserMenu: React.FC = () => {
-  const { logout } = useUser()
+  const { logout, user } = useUser()
 
   return (
-    <DropdownCta triggerClass="user-icon">
-      <nav>
-        <button onClick={logout}>Logout</button>
+    <DropdownCta altTrigger={<Avatar className="trigger" src={user?.photoURL} tabIndex={-1} />}>
+      <nav className="UserMenu">
+          <section className="user-section">
+            <button tabIndex={0} onClick={logout}>
+              {user?.displayName || 'Edit Profile'}
+            </button>
+          </section>
+          <button tabIndex={0}>Bands</button>
+          <button tabIndex={0}>Playlists</button>
+          <section className="log-out-section">
+            <button tabIndex={0} onClick={logout}>Log out</button>
+          </section>
       </nav>
     </DropdownCta>
   )
