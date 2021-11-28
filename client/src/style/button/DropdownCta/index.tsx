542b72ca6c109888f81677e1959fbc82d72bdc2e
Greyson Elkins <greysonelkins@gmail.com>
2021-11-28 12:29:27 -0700

    Allow replacement of Cta

--- client/src/style/button/DropdownCta/index.tsx
+++ client/src/style/button/DropdownCta/index.tsx
@@ -1,27 +1,42 @@
+import { useState, FocusEvent } from 'react'
 import CtaOne from 'style/button/CtaOne'
 
 import './DropdownCta.scss'
 
-// type Props = {
-//   text?: string
-//   triggerProps?: React.DetailedHTMLProps<
-//     React.ButtonHTMLAttributes<HTMLButtonElement>,
-//     HTMLButtonElement
-//   >
-// }
-
-interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+interface Props
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   text?: string
   triggerClass?: string
+  altTrigger?: JSX.Element
 }
 
-const DropdownCta: React.FC<Props> = ({ children, text, triggerClass, ...props }) => {
+const DropdownCta: React.FC<Props> = ({ children, text, triggerClass, altTrigger, ...props }) => {
+  const [override, setOverride] = useState<boolean>(false)
+
+  const overrideHiddenMenu = () => override ? ' override-hide' : ''
+
+  const handleTab = (event: FocusEvent<HTMLDivElement, Element>) => {
+    event.relatedTarget ? setOverride(true) : setOverride(false)
+  }
+
   return (
-    <div className="DropdownCta">
-      <CtaOne className={`${triggerClass || ""}`} {...props}>
-        {text && text}
-      </CtaOne>
-      <nav className={`dropdown`}>{children}</nav>
+    <div
+      className="DropdownCta"
+      tabIndex={0}
+      onFocus={handleTab}
+      onBlur={handleTab}
+    >
+      {altTrigger ? (
+        altTrigger
+      ) : (
+        <CtaOne className={`trigger ${triggerClass || ''}`} {...props}>
+          {text && text}
+        </CtaOne>
+      )}
+      <div className={`dropdown ${overrideHiddenMenu()}`}>{children}</div>
     </div>
   )
 }
