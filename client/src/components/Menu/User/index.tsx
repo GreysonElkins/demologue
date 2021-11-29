fdc2a6853c9cee645a65be9d9d357ab1ddd675ed
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 12:00:13 -0700

    Add another link

--- client/src/components/Menu/User/index.tsx
+++ client/src/components/Menu/User/index.tsx
@@ -12,16 +12,14 @@ const UserMenu: React.FC = () => {
   return (
     <DropdownCta altTrigger={<Avatar className="trigger" src={user?.photoURL} tabIndex={-1} />}>
       <nav className="UserMenu">
-          <section className="user-section">
-            <button>
-              {user?.displayName || 'Edit Profile'}
-            </button>
-          </section>
-          <Link to="/my-bands">Bands</Link>
-          <button>Playlists</button>
-          <section className="log-out-section">
-            <button onClick={logout}>Log out</button>
-          </section>
+        <section className="user-section">
+          <Link to="/user">{user?.displayName || 'Edit Profile'}</Link>
+        </section>
+        <Link to="/my-bands">Bands</Link>
+        <button>Playlists</button>
+        <section className="log-out-section">
+          <button onClick={logout}>Log out</button>
+        </section>
       </nav>
     </DropdownCta>
   )
