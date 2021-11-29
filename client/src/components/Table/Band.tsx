518a002719185ebab769dc3960bf99751b18509f
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 11:58:51 -0700

    Rename

--- client/src/components/Table/Band.tsx
+++ client/src/components/Table/Band.tsx
@@ -3,12 +3,13 @@ import Band from 'types/Band'
 
 import Table from '.'
 import Avatar from 'style/Icon/Avatar'
+import test from 'test/data/bands'
 
-const BandList: React.FC<{bands: Band[]}> = ({ bands }) => {
+const BandList: React.FC<{bands?: Band[]}> = ({ bands = test }) => {
   const data = useMemo(
     () =>
       bands.map(({ name, photoURL }) => ({
-        col1: <Avatar src={photoURL} className="avatar-squared" defaultIcon="drum" size="3x"/>,
+        col1: <Avatar src={photoURL} className="avatar-squared" defaultIcon="drum" size="3x" noAction />,
         col2: name,
       })),
     [bands]
