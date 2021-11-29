e0cbb47f78945a186b61d5d5bf38448534e03275
Greyson Elkins <greysonelkins@gmail.com>
2021-11-28 17:54:20 -0700

    Initialize FontAwesome

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { initFontAwesome } from 'style/Icon/fontAwesome'
 
 import App from 'App'
 
 import 'style/index.scss'
+
+initFontAwesome()
 
 ReactDOM.render(
   <React.StrictMode>
