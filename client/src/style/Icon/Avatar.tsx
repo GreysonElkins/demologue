11f5eeaaf264664327ac813560eff1eaf103278a
Greyson Elkins <greysonelkins@gmail.com>
2021-11-28 17:55:51 -0700

    Use icon instead of emoji

--- client/src/style/Icon/Avatar.tsx
+++ client/src/style/Icon/Avatar.tsx
@@ -1,14 +1,19 @@
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core'
 import { DetailedHTMLProps } from 'react'
+import { Icon } from 'style/Icon'
 
 import './index.scss'
 
 interface Props extends DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   src?: string | null
+  defaultIcon?: IconProp
+  size?: SizeProp
 }
 
-const Avatar: React.FC<Props> = ({ src, className, ...props }) => (
-  <button data-ico="👤" className={`Avatar ${className || ''}`} {...props} style={src ? { background: `url(${src})` } : {}}>
-    {/* {!src && <Icon icon="user" />} */}
+const Avatar: React.FC<Props> = ({ src, defaultIcon, className, size, ...props }) => (
+  <button className={`Avatar ${className || ''}`} {...props}>
+    {src && <img src={src} alt="" />}
+    {!src && <Icon icon={ defaultIcon || "user" } size={ size || "1x"} />}
   </button>
 )
 
