b162b4a67ffdb3631217a6c9d140572d0d025f45
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 12:02:22 -0700

    Add home and headphones

--- client/src/style/Icon/fontAwesome.ts
+++ client/src/style/Icon/fontAwesome.ts
@@ -3,10 +3,20 @@ import {
   faSortUp,
   faSortDown,
   faDrum,
-  faSave
+  faSave,
+  faHome,
+  faHeadphones
 } from '@fortawesome/free-solid-svg-icons'
 import { library, IconDefinition } from '@fortawesome/fontawesome-svg-core'
 
-const fontAwesomeIconList: IconDefinition[] = [faUser, faSortUp, faSortDown, faDrum, faSave]
+const fontAwesomeIconList: IconDefinition[] = [
+  faUser,
+  faSortUp,
+  faSortDown,
+  faDrum,
+  faSave,
+  faHome,
+  faHeadphones,
+]
 
 export const initFontAwesome = () => library.add(...fontAwesomeIconList)
