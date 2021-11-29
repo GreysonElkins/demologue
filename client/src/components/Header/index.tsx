7a43322b70fbc78772cca26f2bacb2593aa6ecaf
Greyson Elkins <greysonelkins@gmail.com>
2021-11-28 17:54:36 -0700

    Turn header into link

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
 import { useUser } from 'context/User'
 
 import UserMenu from '../Menu/User'
 import SignInCta from './SignInCta'
+import { Icon } from 'style/Icon'
 
 import './Header.scss'
 
@@ -9,7 +11,7 @@ const Header: React.FC = () => {
   const { signedIn, loading } = useUser()
   return (
     <header>
-      <h1>Demologue</h1>
+      <h1><Link to="/"><Icon icon="save" />Demologue</Link></h1>
       {!loading && (signedIn ? <UserMenu /> : <SignInCta />)}
     </header>
   )
