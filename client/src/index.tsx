a8d35e6e841757bbd8abdfcf95a49fbe21f0c470
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 18:08:06 -0700

    Add toast

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ToastContainer } from 'react-toastify'
 import { initFontAwesome } from 'style/Icon/fontAwesome'
 
 import App from 'App'
 
 import 'style/index.scss'
+import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 initFontAwesome()
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="bottom-right" />
   </React.StrictMode>,
   document.getElementById('root')
 )
