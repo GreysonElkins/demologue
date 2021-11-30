bc5c9a7ee73a2041ae2dd78d16e201d2db48c900
Greyson Elkins <greysonelkins@gmail.com>
2021-11-30 12:08:10 -0700

    Add GetBandById

--- client/src/scripts/api/demologue/query/band.ts
+++ client/src/scripts/api/demologue/query/band.ts
@@ -1,5 +1,5 @@
 import endpoint from 'scripts/api/demologue'
-import { GET_BANDS_BY_USER } from 'scripts/api/demologue/query/band.gql'
+import { GET_BANDS_BY_USER, GET_BAND_BY_ID } from 'scripts/api/demologue/query/band.gql'
 import { useQuery } from 'react-query'
 import { request } from 'graphql-request'
 
@@ -7,9 +7,19 @@ export const getBandsByUser = (userId?: string) => {
   return useQuery(
     'user-to-bands',
     async () => {
-      const { usersToBands: data } : any = await request(endpoint, GET_BANDS_BY_USER, { userId })
+      const { usersToBands: data }: any = await request(endpoint, GET_BANDS_BY_USER, { userId })
       return data
     },
     { enabled: !!userId }
+  )
+}
+
+export const getBandById = (id: number) => {
+  return useQuery(
+    'get-band-by-id',
+    async () => {
+      const { bandById: data } :any = await request(endpoint, GET_BAND_BY_ID, { id })
+      return data
+    }
   )
 }
