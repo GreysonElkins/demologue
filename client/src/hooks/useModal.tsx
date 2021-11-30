364c57295c4b9fcc499d6b8ff6954d6f43d1eac0
Greyson Elkins <greysonelkins@gmail.com>
2021-11-30 12:07:34 -0700

    Add an optional param for second flag on toggle

--- client/src/hooks/useModal.tsx
+++ client/src/hooks/useModal.tsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const useModal = (options?:{ defaultIsOpen?: boolean }) => {
   const [isOpen, setIsOpen] = useState<boolean>(options?.defaultIsOpen || false)
+  const [secondOption, setSecondOption] = useState<any>(undefined)
 
-  const toggle = () => setIsOpen(prev => !prev)
+  const toggle = (optionalParam?: any) => {
+    if (optionalParam !== undefined && secondOption !== optionalParam) {
+      setSecondOption(optionalParam)
+    } 
+    setIsOpen(prev => !prev)
+  }
 
-  return { isOpen, toggle }
+  return { isOpen, toggle, secondOption }
 }
 
 export default useModal
