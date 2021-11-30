7f24c2447925a8527c656903b83745440636b1b1
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 18:02:45 -0700

    Rename

---
+++ client/src/style/form/StyledField.tsx
@@ -0,0 +1,32 @@
+import { Field, FieldAttributes, ErrorMessage } from 'formik'
+
+import './index.scss'
+
+export const printDropdwonOptions = (options: Array<{text: string, value: any}>) => options.map(({ text, value }, i) => (
+  <option key={`option-${i}:${text}`} value={value}>{text}</option>
+))
+
+const StyledField: React.FC<FieldAttributes<any>> = ({ label, ...props }) => {
+  const className = () => {
+    switch(props.as) {
+      default: return 'LineText'
+  }}
+  return (
+    <div className="StyledField">
+      {label && (
+        <label className="Label" htmlFor={props.name}>
+          {label}
+          {props.required && <span>{"*"}</span>}
+        </label>
+      )}
+      <div>
+      <Field className={className()} {...props} />
+      </div>
+      <div className="ErrorWrapper">
+        <ErrorMessage name={props.name || ''} component="span" />
+      </div>
+    </div>
+  )
+}
+
+export default StyledField
