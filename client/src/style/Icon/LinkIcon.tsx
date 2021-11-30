dbe243d7edb87edb6c2e5da0505ebfa3a8496d11
Greyson Elkins <greysonelkins@gmail.com>
2021-11-29 18:02:58 -0700

    Remove unused import

--- client/src/style/Icon/LinkIcon.tsx
+++ client/src/style/Icon/LinkIcon.tsx
@@ -1,4 +1,4 @@
-import { NavLink, NavLinkProps } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import { Icon } from 'style/Icon'
 import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
