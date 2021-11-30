9ca5f3365906af47f6a064d795452d76031e21aa
Greyson Elkins <greysonelkins@gmail.com>
2021-11-30 12:06:02 -0700

    Add a right side container with additional Cta

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { useUser } from 'context/User'
 import UserMenu from '../Menu/User'
 import SignInCta from './SignInCta'
 import { Icon } from 'style/Icon'
+import Cta from 'style/button/Cta'
 
 import './Header.scss'
 
@@ -11,8 +12,23 @@ const Header: React.FC = () => {
   const { signedIn, loading } = useUser()
   return (
     <header>
-      <h1><Link to="/"><Icon icon="save" />Demologue</Link></h1>
-      {!loading && (signedIn ? <UserMenu /> : <SignInCta />)}
+      <h1>
+        <Link to="/">
+          <Icon icon="save" />
+          Demologue
+        </Link>
+      </h1>
+      {!loading &&
+        (signedIn ? (
+          <div className="right">
+            <Cta ico="⬆" className="CtaOne collapse">
+              Upload a Track
+            </Cta>
+            <UserMenu />
+          </div>
+        ) : (
+          <SignInCta />
+        ))}
     </header>
   )
 }
