1facf0e2e8978391d110fdf5cd05e4b786acce67
Greyson Elkins <greysonelkins@gmail.com>
2021-11-30 12:07:55 -0700

    Add getBandById

--- client/src/scripts/api/demologue/query/band.gql.ts
+++ client/src/scripts/api/demologue/query/band.gql.ts
@@ -10,3 +10,20 @@ export const GET_BANDS_BY_USER = gql`
     }
   }
 `
+
+export const GET_BAND_BY_ID = gql`
+  query GetBandsById($id: Int!) {
+    bandById(id: $id) {
+      id
+      name
+      usersToBands(condition: { role: MEMBER }) {
+        user {
+          photoUrl
+          displayName
+        }
+      }
+      photoUrl
+      createdAt
+    }
+  }
+`
