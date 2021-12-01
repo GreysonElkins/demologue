2f3ce85702189fd742b8a62ab44526a0d9f27d53
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 16:39:00 -0700

    Add get bands by ids

--- client/src/scripts/api/demologue/query/band.ts
+++ client/src/scripts/api/demologue/query/band.ts
@@ -1,7 +1,9 @@
 import endpoint from 'scripts/api/demologue'
-import { GET_BANDS_BY_USER, GET_BAND_BY_ID } from 'scripts/api/demologue/query/band.gql'
+import { GET_BANDS_BY_USER, GET_BAND_BY_ID, GET_BANDS_BY_IDS } from 'scripts/api/demologue/query/band.gql'
 import { useQuery } from 'react-query'
 import { request } from 'graphql-request'
+
+import type { gqlBand } from 'types/Band'
 
 export const getBandsByUser = (userId?: string) => {
   return useQuery(
@@ -14,12 +16,32 @@ export const getBandsByUser = (userId?: string) => {
   )
 }
 
-export const getBandById = (id: number) => {
+type ByIdResponse = {
+  bandById: gqlBand
+}
+
+export const getBandById = (id: number | null) => {
   return useQuery(
     'get-band-by-id',
     async () => {
-      const { bandById: data } :any = await request(endpoint, GET_BAND_BY_ID, { id })
+      const { bandById: data } :ByIdResponse = await request(endpoint, GET_BAND_BY_ID, { id })
       return data
-    }
+    },
+    { enabled: !!id }
+  )
+}
+
+type ByIdsResponse = {
+  bandsByIds: gqlBand[]
+}
+
+export const getBandsByIds = (ids: number[] | null) => {
+  return useQuery(
+    'get-bands-by-ids',
+    async () => {
+      const { bandsByIds }: ByIdsResponse = await request(endpoint, GET_BANDS_BY_IDS, { ids })
+      return bandsByIds
+    },
+    { enabled: !!ids }
   )
 }
