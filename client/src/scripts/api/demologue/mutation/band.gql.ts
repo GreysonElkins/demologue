0b214fdb807c553ab8dcc9e4a4fb3fb004e727c6
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 16:37:48 -0700

    Use boilerplate gqlBand

--- client/src/scripts/api/demologue/mutation/band.gql.ts
+++ client/src/scripts/api/demologue/mutation/band.gql.ts
@@ -1,19 +1,22 @@
 import { gql } from 'graphql-request'
+import { gqlBand } from '../query/band.gql'
 
 export const CREATE_BAND = gql`
   mutation CreateBand($name: String!) {
     createBand(input: { band: { name: $name } }) {
       band {
-        id
+        ${gqlBand}
       }
     }
   }
 `
 
-export const CREATE_USERS_TO_BAND= gql`
+export const CREATE_USERS_TO_BAND = gql`
   mutation CreateUsersToBand($bandId: Int!, $userId: String!, $role: UserRole!) {
     createUsersToBand(input: { usersToBand: { userId: $userId, bandId: $bandId, role: $role } }) {
-      clientMutationId
+      band {
+        ${gqlBand}
+      }
     }
   }
 `
