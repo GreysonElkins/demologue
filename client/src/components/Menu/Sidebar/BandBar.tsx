d2cccb87727bdc46ca22c07a956a4821d06d1351
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 16:35:46 -0700

    Refer to bands in context

--- client/src/components/Menu/Sidebar/BandBar.tsx
+++ client/src/components/Menu/Sidebar/BandBar.tsx
@@ -1,32 +1,35 @@
+import { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 import useModal from 'hooks/useModal'
 import { useViewer } from 'context/Viewer'
-import { getBandsByUser } from 'scripts/api/demologue/query/band'
+import { useBands } from 'context/Bands'
 
+import Band from 'types/Band'
 import AddBand from 'components/Modal/AddBand'
 
 import Cta from 'style/button/Cta'
-import Loading from 'style/Icon/Loading'
 
 const BandBar = () => {
   const { toggle, isOpen, secondOption: skipOptions } = useModal()
   const { user } = useViewer()
-  const { status, data, isFetching } = getBandsByUser(user?.uid)
+  const userBands = useMemo(() => user?.bandIds() || [], [JSON.stringify(user)])
+  const { bands } = useBands(userBands)
 
-  const printBands = () =>
-    data.map(({ band: { id, name } }: any, i: number) => (
-      <li key={`user-band-${i}`}>
-        <Link to={`/band/${id}`}>{name}</Link>
-      </li>
-    ))
+  const ListItem = ({ id, name }: Band) =>
+    <li key={`user-band-list-${id}`}>
+      <Link to={`/band/${id}`}>{name}</Link>
+    </li>
+
+  const printBands = Object.values(bands).reduce((ui, band) => {
+      if (band && userBands?.includes(band.id)) ui.push(ListItem(band))
+      return ui
+    }, [] as JSX.Element[])
 
   return (
     <>
       <AddBand toggle={toggle} isOpen={isOpen} skipOptions={skipOptions} />
       <div className="MenuContents">
-        {isFetching && <Loading />}
-        {/* it'd be cool to do a graphic on the selection bar for loading instead */}
-        {status === 'success' && data.length === 0 && (
+        {userBands && userBands.length === 0 && (
           <div className="no-items">
             It looks like you don't have any bands <br />{' '}
             <Link to="/search-bands">join a band</Link> or{' '}
@@ -41,7 +44,7 @@ const BandBar = () => {
             </i>
           </div>
         )}
-        {data?.length > 0 && <ul>{printBands()}</ul>}
+        {userBands && userBands.length > 0 && <ul>{printBands}</ul>}
         <div className="bottom">
           <Cta cta="Two" ico="➕" className="CtaTwo collapse" onClick={() => toggle(false)}>
             Add a Band
