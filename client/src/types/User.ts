5270c0e520759a66f84a5bd07b9acd198d8e00d5
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 16:40:23 -0700

    Add rolemap type

--- client/src/types/User.ts
+++ client/src/types/User.ts
@@ -9,21 +9,26 @@ export type gqlUser = {
   usersToBands: Array<{ role: BandRole; bandId: number }>
 }
 
+type RoleMap = { [key: number]: BandRole }
+
 class User {
     uid: string
     displayName: string
     photoUrl: string | null
     email: string | null // may be null with certain types of auth?
-    bands: Array<{
-      [key:number]: BandRole
-    }>
+    bands: RoleMap
     constructor ({ displayName, email, photoUrl, uid, usersToBands }: gqlUser) {
       this.uid = uid
       this.displayName = displayName
       this.photoUrl = photoUrl
       this.email = email
-      this.bands = usersToBands.map(({ bandId, role }) => ({ [bandId]: role }))
+      this.bands = usersToBands.reduce(
+        (bands, { bandId, role }) => ({ ...bands, [bandId]: role }),
+        {} as RoleMap
+      )
     }
+
+    bandIds = () => Object.keys(this.bands).map(id => Number(id))
 }
 
 export default User
