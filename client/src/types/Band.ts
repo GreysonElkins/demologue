e978fec298168187453fe32dd1ebd2fa6aa97b27
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 16:39:34 -0700

    Add constructor

--- client/src/types/Band.ts
+++ client/src/types/Band.ts
@@ -1,9 +1,26 @@
 export type BandRole = "MEMBER" | "SUPPORT" | "GUEST"
 
-type Band = {
+export type gqlBand = {
   id: number
   name: string
-  photoURL?: string | null
+  photoUrl: string | null
+  createdAt: string
+  usersToBands: Array<{ role: BandRole; userId: number }>
+}
+
+class Band {
+  id: number
+  name: string
+  photoUrl: string | null
+  createdAt: Date
+  members: Array<{ [key: string]: BandRole }>
+  constructor({ id, name, photoUrl, createdAt, usersToBands }: gqlBand) {
+    this.id = id
+    this.name = name
+    this.photoUrl = photoUrl
+    this.createdAt = new Date(createdAt)
+    this.members = usersToBands.map(({ role, userId }) => ({ [userId]: role }))
+  }
 }
 
 export default Band
