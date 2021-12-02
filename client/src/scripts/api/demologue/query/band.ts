4cf6385928aa33f124f883d22943ddc2d06a3f5b
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 19:44:54 -0700

    Remove unused query

--- client/src/scripts/api/demologue/query/band.ts
+++ client/src/scripts/api/demologue/query/band.ts
@@ -1,20 +1,9 @@
 import endpoint from 'scripts/api/demologue'
-import { GET_BANDS_BY_USER, GET_BAND_BY_ID, GET_BANDS_BY_IDS } from 'scripts/api/demologue/query/band.gql'
+import { GET_BAND_BY_ID, GET_BANDS_BY_IDS } from 'scripts/api/demologue/query/band.gql'
 import { useQuery } from 'react-query'
 import { request } from 'graphql-request'
 
 import type { gqlBand } from 'types/Band'
-
-export const getBandsByUser = (userId?: string) => {
-  return useQuery(
-    'user-to-bands',
-    async () => {
-      const { usersToBands: data }: any = await request(endpoint, GET_BANDS_BY_USER, { userId })
-      return data
-    },
-    { enabled: !!userId }
-  )
-}
 
 type ByIdResponse = {
   bandById: gqlBand
