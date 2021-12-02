1cf2c46e65148d9b000b1357d11d0bdec0f4af7f
Greyson Elkins <greysonelkins@gmail.com>
2021-12-01 19:45:46 -0700

    Add UserMap type

--- client/src/types/Band.ts
+++ client/src/types/Band.ts
@@ -8,18 +8,20 @@ export type gqlBand = {
   usersToBands: Array<{ role: BandRole; userId: number }>
 }
 
+type UserMap = { [key: string]: BandRole } 
+
 class Band {
   id: number
   name: string
   photoUrl: string | null
   createdAt: Date
-  members: Array<{ [key: string]: BandRole }>
+  members: UserMap
   constructor({ id, name, photoUrl, createdAt, usersToBands }: gqlBand) {
     this.id = id
     this.name = name
     this.photoUrl = photoUrl
     this.createdAt = new Date(createdAt)
-    this.members = usersToBands.map(({ role, userId }) => ({ [userId]: role }))
+    this.members = usersToBands.reduce((map, { role, userId }) => ({ ...map, [userId]: role }), {} as UserMap)
   }
 }
 
