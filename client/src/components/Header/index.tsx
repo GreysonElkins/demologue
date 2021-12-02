f4bae022d35f09342b126a33be31a00aac30890e
Greyson Elkins <greysonelkins@gmail.com>
2021-12-02 15:31:48 -0700

    Add upload track modal

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,35 +1,42 @@
 import { Link } from 'react-router-dom'
 import { useViewer } from 'context/Viewer'
+import useModal from 'hooks/useModal'
 
 import UserMenu from '../Menu/User'
 import SignInCta from './SignInCta'
 import { Icon } from 'style/Icon'
 import Cta from 'style/button/Cta'
 
+import UploadTrack from 'components/Modal/UploadTrack'
+
 import './Header.scss'
 
 const Header: React.FC = () => {
   const { signedIn, loading } = useViewer()
+  const { toggle, isOpen } = useModal()
   return (
-    <header>
-      <h1>
-        <Link to="/">
-          <Icon icon="save" />
-          Demologue
-        </Link>
-      </h1>
-      {!loading &&
-        (signedIn ? (
-          <div className="right">
-            <Cta ico="⬆" className="CtaOne collapse">
-              Upload a Track
-            </Cta>
-            <UserMenu />
-          </div>
-        ) : (
-          <SignInCta />
-        ))}
-    </header>
+    <>
+      <UploadTrack toggle={toggle} isOpen={isOpen} />
+      <header>
+        <h1>
+          <Link to="/">
+            <Icon icon="save" />
+            Demologue
+          </Link>
+        </h1>
+        {!loading &&
+          (signedIn ? (
+            <div className="right">
+                <Cta ico="⬆" className="CtaOne collapse" onClick={toggle}>
+                  Upload a Track
+                </Cta>
+              <UserMenu />
+            </div>
+          ) : (
+            <SignInCta />
+          ))}
+      </header>
+    </>
   )
 }
 
