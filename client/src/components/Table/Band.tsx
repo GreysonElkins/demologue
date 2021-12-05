46659cbe379897d61b6279cbcbb6491e5aa50ec9
Greyson Elkins <greysonelkins@gmail.com>
2021-12-05 13:58:32 -0700

    Remove unused test references

--- client/src/components/Table/Band.tsx
+++ client/src/components/Table/Band.tsx
@@ -3,18 +3,18 @@ import Band from 'types/Band'
 
 import Table from '.'
 import Avatar from 'style/Icon/Avatar'
-import test from 'test/data/bands'
+// import test from 'test/data/bands'
 
-const BandList: React.FC<{bands?: Band[]}> = ({ bands = test }) => {
+const BandList: React.FC<{bands?: Band[]}> = ({ bands }) => {
   const data = useMemo(
     () =>
-      bands.map(({ name, photoUrl }) => ({
+      bands?.map(({ name, photoUrl }) => ({
         col1: <Avatar src={photoUrl} className="avatar-squared" defaultIcon="drum" size="3x" noAction />,
         col2: name,
         col3: "testData",
         col4: <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinclipart.com%2Fpindetail%2FThwhxi_sound-wave-clipart-soud-audio-sound-wave-png%2F&psig=AOvVaw0PmiznWuyJDReBGAkCOWoU&ust=1638386044401000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjl0uTlwPQCFQAAAAAdAAAAABAK" />
 
-      })),
+      })) || [],
     [bands]
   )
   const columns = useMemo(() => [{
