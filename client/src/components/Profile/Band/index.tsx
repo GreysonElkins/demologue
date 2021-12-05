9760a32a37717ccaeb8277df6bde528fd0ab4fcb
Greyson Elkins <greysonelkins@gmail.com>
2021-12-05 13:58:11 -0700

    Start member sections

--- client/src/components/Profile/Band/index.tsx
+++ client/src/components/Profile/Band/index.tsx
@@ -4,6 +4,7 @@ import { useBands } from 'context/Bands'
 import Loading from 'style/Icon/Loading'
 import Header from './Header'
 import './BandProfile.scss'
+import CollectionGallery from './CollectionGallery'
 
 const BandProfile = () => {
   const { bandId } = useParams()
@@ -15,6 +16,10 @@ const BandProfile = () => {
   return (
     <div className="BandProfile">
       <Header band={match} />
+      {/* OWNER SECTIONS */}
+      <div>
+        <CollectionGallery band={match} onSelectCollection={(id) => console.log(id)}/>
+      </div>
     </div>
   )
 }
