ca81fd0098cf8e6a1497e74acaaafea3f1cad51d
Greyson Elkins <greysonelkins@gmail.com>
2021-12-09 17:27:55 -0700

    Create track list

--- client/src/types/Band.d.ts
+++ client/src/types/Band.d.ts
@@ -1,3 +1,4 @@
+import Collection, { gqlCollection } from './Collection.d'
 export type BandRole = "MEMBER" | "SUPPORT" | "GUEST"
 
 export type gqlBand = {
@@ -6,7 +7,8 @@ export type gqlBand = {
   photoUrl: string | null
   createdAt: string
   usersToBands: Array<{ role: BandRole; userId: number }>
-  collectionsByCreatedByBand: number[]
+  collectionsByCreatedByBand: gqlCollection[]
+  tracks: Array<{ id: number, updatedAt: string }>
 }
 
 type RoleMap = { [key: string]: BandRole } 
@@ -17,14 +19,27 @@ class Band {
   photoUrl: string | null
   createdAt: Date
   members: RoleMap
-  collections: Array<{ id: number, name: string }>
-  constructor({ id, name, photoUrl, createdAt, usersToBands, collectionsByCreatedByBand }: gqlBand) {
+  collections: Collection[]
+  tracks: Array<{ id: number; updatedAt: string }>
+  constructor({
+    id,
+    name,
+    photoUrl,
+    createdAt,
+    usersToBands,
+    collectionsByCreatedByBand,
+    tracks,
+  }: gqlBand) {
     this.id = id
     this.name = name
     this.photoUrl = photoUrl
     this.createdAt = new Date(createdAt)
-    this.members = usersToBands.reduce((map, { role, userId }) => ({ ...map, [userId]: role }), {} as RoleMap)
-    this.collections = collectionsByCreatedByBand
+    this.members = usersToBands.reduce(
+      (map, { role, userId }) => ({ ...map, [userId]: role }),
+      {} as RoleMap
+    )
+    this.collections = collectionsByCreatedByBand.map((collection) => new Collection(collection))
+    this.tracks = tracks
   }
 }
 
