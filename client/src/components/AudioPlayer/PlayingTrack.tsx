4b534519386ce3d17a2fedc5620b05bbb531176a
Greyson Elkins <greysonelkins@gmail.com>
2021-12-10 14:30:11 -0700

    Add song info to player

--- client/src/components/AudioPlayer/PlayingTrack.tsx
+++ client/src/components/AudioPlayer/PlayingTrack.tsx
@@ -1,8 +1,18 @@
+import { useBands } from 'context/Bands'
 import { usePlayer } from 'context/Player'
 
 const PlayingTrack: React.FC = () => {
-  const { wave } = usePlayer()
-  return <div className="PlayingTrack"><div ref={wave} /></div>
+  const { wave, trackInfo } = usePlayer()
+  const { match: band } = useBands(trackInfo?.bandId)
+  return (
+    <div className="PlayingTrack">
+      <div ref={wave} />
+      <div className="track-info">
+        <i>{trackInfo?.title || trackInfo?.workingTitle || 'Untitled'}</i> -{' '}
+        <b style={{ textTransform: 'capitalize' }}>{band?.name}</b>
+      </div>
+    </div>
+  )
 }
 
 export default PlayingTrack
