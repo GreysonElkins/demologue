2c12b76c1acbcd5455bd0ae447f5eeedbb9ec4a9
Greyson Elkins <greysonelkins@gmail.com>
2021-12-09 17:27:29 -0700

    Add cloudinaryId to track parsing

--- client/src/scripts/api/demologue/mutation/track.ts
+++ client/src/scripts/api/demologue/mutation/track.ts
@@ -1,25 +1,40 @@
-import request from "graphql-request"
-import { useMutation } from "react-query"
-import { toast } from "react-toastify"
-import endpoint from "scripts/api/demologue"
-import { CREATE_TRACK } from "./track.gql"
+import request from 'graphql-request'
+import { useMutation } from 'react-query'
+import { toast } from 'react-toastify'
+import endpoint from 'scripts/api/demologue'
+import { CREATE_TRACK } from './track.gql'
 
 type NewTrackData = {
-  url: string,
+  url: string
   title?: string
   workingTitle?: string
   bandId: number
   userId: string
+  cloudinaryId: string
 }
 
-export const createTrack = () => 
+export const createTrack = () =>
   useMutation(
-    async({ url: trackUrl, title, workingTitle, bandId, userId: uploadedBy }: NewTrackData) => {
+    async ({
+      url: trackUrl,
+      title,
+      workingTitle,
+      bandId,
+      userId: uploadedBy,
+      cloudinaryId,
+    }: NewTrackData) => {
       try {
-        const { createTrack: track } = await request(endpoint, CREATE_TRACK, { trackUrl, title, workingTitle, bandId, uploadedBy })
+        const { createTrack: track } = await request(endpoint, CREATE_TRACK, {
+          trackUrl,
+          title,
+          workingTitle,
+          bandId,
+          uploadedBy,
+          cloudinaryId,
+        })
         return track
       } catch (error) {
-        toast.error("Something went wrong while saving your track, please try again")
+        toast.error('Something went wrong while saving your track, please try again')
         console.error(error)
       }
     }
