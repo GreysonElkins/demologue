96e8d36b2e11d941e188a0963ae4fb4491b88008
Greyson Elkins <greysonelkins@gmail.com>
2021-12-10 14:31:00 -0700

    Refer to track context for playing tracks

--- client/src/context/Player.tsx
+++ client/src/context/Player.tsx
@@ -1,13 +1,24 @@
-import { createContext, useContext, useState, useRef, useCallback, MutableRefObject } from 'react'
+import {
+  createContext,
+  useContext,
+  useState,
+  useRef,
+  useCallback,
+  MutableRefObject,
+  useEffect,
+} from 'react'
 import WaveSurfer from 'wavesurfer.js'
 import AudioPlayer from 'components/AudioPlayer'
+import { useTracks } from './Tracks'
+import Track from 'types/Track'
 
 type PlayerContextValue = {
   isMounted: boolean
   isPlaying: boolean
-  mountTrack: (url: string | null) => void
+  mountTrack: (trackId: number) => void
   playPause: () => void
   wave: MutableRefObject<HTMLDivElement | null> | null
+  trackInfo: Track | null
 }
 
 const formWaveSurferOptions = (ref: any) => ({
@@ -17,7 +28,7 @@ const formWaveSurferOptions = (ref: any) => ({
   cursorColor: '#1a72d8',
   barWidth: 2,
   barRadius: 3,
-  height: 40,
+  height: 30,
   width: '100%',
   normalize: true,
   partialRender: true,
@@ -31,35 +42,58 @@ const PlayerContext = createContext({} as PlayerContextValue)
 export const PlayerProvider: React.FC = ({ children }) => {
   const [isMounted, setIsMounted] = useState<boolean>(false)
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const [selectedTrack, selectTrack] = useState<number | undefined>(undefined)
+  const { match: track } = useTracks(selectedTrack)
   const wave = useRef<HTMLDivElement | null>(null)
   const audio = useRef<WaveSurfer | null>(null)
 
   const playPause = useCallback(() => {
     audio.current?.playPause()
-    setIsPlaying((prev) => !prev)
   }, [])
 
-  const setInternalRef = useCallback((url: string) => {
-    if (!url) return
+  useEffect(() => {
+    if (!track) return
+    setInternalRef()
+  }, [track])
+
+  const handleAudioEvents = () => {
+    audio.current?.on('ready', () => {
+      setIsMounted(true)
+      playPause()
+    })
+    audio.current?.on('play', () => setIsPlaying(true))
+    audio.current?.on('pause', () => setIsPlaying(false))
+  }
+
+  const handleKeyboardEvents = (event: KeyboardEvent) => {
+    if (event.code ===  "Space") {
+      playPause()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keyup', event => handleKeyboardEvents(event))
+    return window.removeEventListener('keyup', event => handleKeyboardEvents(event))
+  }, [])
+
+  const setInternalRef = useCallback(() => {
     audio.current?.destroy()
+    if (!track || !track.trackUrl) return
     const options = formWaveSurferOptions(wave.current)
     audio.current = WaveSurfer.create(options)
-
-    audio.current.load(url)
-    setIsMounted(true)
-
+    handleAudioEvents()
+    audio.current.load(track.trackUrl)
     return () => audio.current?.destroy()
-  }, [])
+  }, [track])
 
-  const mountTrack = (url: string | null) => {
-    if (!url) return // maybe stop playing and unmount?
-    setInternalRef(url)
+  const mountTrack = (trackId: number) => {
+    if (!trackId) return // maybe stop playing and unmount?
+    selectTrack(trackId)
   }
 
   return (
-    <PlayerContext.Provider value={{ isMounted, mountTrack, playPause, isPlaying, wave }}>
+    <PlayerContext.Provider value={{ isMounted, mountTrack, playPause, isPlaying, wave, trackInfo: track }}>
       {children}
-      {/* <AudioPlayer>{Wave}</AudioPlayer> */}
       <AudioPlayer />
     </PlayerContext.Provider>
   )
