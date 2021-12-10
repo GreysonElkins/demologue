9c3ca374624dedeb497df7b207a0c1fa6655426e
Greyson Elkins <greysonelkins@gmail.com>
2021-12-09 17:27:09 -0700

    Type cloud response

--- client/src/scripts/api/cloudinary/index.ts
+++ client/src/scripts/api/cloudinary/index.ts
@@ -2,6 +2,11 @@ import Preset from "types/CloudinaryPresets.d"
 const endpoint = `https://api.cloudinary.com/v1_1/demologue`
 import { toast } from 'react-toastify'
 
+export type CloudResponse = {
+  url: string
+  cloudinaryId: string
+}
+
 export const uploadFile = async (file: any, preset: Preset) => {
   const type = preset === Preset.IMAGE ? "image" : "video"
   try {
@@ -13,10 +18,10 @@ export const uploadFile = async (file: any, preset: Preset) => {
       body: data
     })
     const stream = await response.text()
-    const { secure_url } = await JSON.parse(stream)
-    return secure_url
+    const { secure_url: url, public_id: cloudinaryId } = await JSON.parse(stream)
+    return { url, cloudinaryId } as CloudResponse
   } catch (error) {
-    toast.error(`Something went wrong uploading your ${type}, please try again`)
+    toast.error(`Something went wrong uploading your ${type === "image" ? type : "audio"}, please try again`)
     console.error(error)
   }
 }
