8b2e81ac7baf997db073c9582e991b18a984a148
Greyson Elkins <greysonelkins@gmail.com>
2021-12-10 12:54:47 -0700

    Handle mounted track and waveform in context

--- client/src/context/Player.tsx
+++ client/src/context/Player.tsx
@@ -1,52 +1,65 @@
-import { createContext, useContext, useState, useEffect, useRef, useCallback, MutableRefObject } from 'react'
+import { createContext, useContext, useState, useRef, useCallback, MutableRefObject } from 'react'
 import WaveSurfer from 'wavesurfer.js'
 import AudioPlayer from 'components/AudioPlayer'
 
 type PlayerContextValue = {
   isMounted: boolean
   isPlaying: boolean
-  handleWaveformClick: (audioRef: WaveSurfer | null) => void
+  mountTrack: (url: string | null) => void
   playPause: () => void
+  wave: MutableRefObject<HTMLDivElement | null> | null
 }
+
+const formWaveSurferOptions = (ref: any) => ({
+  container: ref,
+  waveColor: '#95a0a5',
+  progressColor: '#53f591',
+  cursorColor: '#1a72d8',
+  barWidth: 2,
+  barRadius: 3,
+  height: 40,
+  width: '100%',
+  normalize: true,
+  partialRender: true,
+  responsive: true,
+  fillParent: true,
+  minPxPerSec: 80,
+})
 
 const PlayerContext = createContext({} as PlayerContextValue)
 
 export const PlayerProvider: React.FC = ({ children }) => {
   const [isMounted, setIsMounted] = useState<boolean>(false)
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
-  const playingRef = useRef<WaveSurfer | null>(null)
-  const [volume, setVolume] = useState<number>(1)
-
-  const checkMount = useCallback(() => {
-    if (playingRef.current && !isMounted) setIsMounted(true)
-    if (!playingRef.current && isMounted) setIsMounted(false)
-  }, [playingRef.current])
+  const wave = useRef<HTMLDivElement | null>(null)
+  const audio = useRef<WaveSurfer | null>(null)
 
   const playPause = useCallback(() => {
-    playingRef.current?.playPause()
-    setIsPlaying(prev => !prev)
+    audio.current?.playPause()
+    setIsPlaying((prev) => !prev)
   }, [])
 
-  const handleWaveformClick = (
-    audioRefCurrent: WaveSurfer | null
-  ) => {
-    if (playingRef.current && audioRefCurrent !== playingRef.current) {
-      // switching off old player
-      {isPlaying && playPause()}
-      playingRef.current = null
-    }
-    if (!playingRef.current) {
-      playingRef.current = audioRefCurrent
-      playPause()
-    }
-    checkMount()
+  const setInternalRef = useCallback((url: string) => {
+    if (!url) return
+    audio.current?.destroy()
+    const options = formWaveSurferOptions(wave.current)
+    audio.current = WaveSurfer.create(options)
+
+    audio.current.load(url)
+    setIsMounted(true)
+
+    return () => audio.current?.destroy()
+  }, [])
+
+  const mountTrack = (url: string | null) => {
+    if (!url) return // maybe stop playing and unmount?
+    setInternalRef(url)
   }
 
   return (
-    <PlayerContext.Provider
-      value={{ isMounted, handleWaveformClick, playPause, isPlaying }}
-    >
+    <PlayerContext.Provider value={{ isMounted, mountTrack, playPause, isPlaying, wave }}>
       {children}
+      {/* <AudioPlayer>{Wave}</AudioPlayer> */}
       <AudioPlayer />
     </PlayerContext.Provider>
   )
