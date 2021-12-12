3809a45fe7956cd67933ff1fe54f7a34e0158341
Greyson Elkins <greysonelkins@gmail.com>
2021-12-11 18:33:47 -0700

    Allow passing class declarations to label

--- client/src/style/Icon/BandAvatar.tsx
+++ client/src/style/Icon/BandAvatar.tsx
@@ -7,9 +7,10 @@ import './Avatar.scss'
 type Props = {
   hideLabel?: boolean
   bandId: number
+  labelClass?: string
 }
 
-const BandAvatar: React.FC<Props> = ({ hideLabel, bandId }) => {
+const BandAvatar: React.FC<Props> = ({ hideLabel, bandId, labelClass }) => {
   const { match } = useBands(bandId)
   if (!match) return <></>
   return (
@@ -18,7 +19,7 @@ const BandAvatar: React.FC<Props> = ({ hideLabel, bandId }) => {
         {match.photoUrl && <img src={match.photoUrl} alt={`${match.name}'s avatar`} />}
         {!match.photoUrl && <Icon icon={'drum'} />}
       </Link>
-      {!hideLabel && <label htmlFor={`${match.name}-avatar`}>{match.name}</label>}
+      {!hideLabel && <label className={`${labelClass ? labelClass : ''}`} htmlFor={`${match.name}-avatar`}>{match.name}</label>}
     </div>
   )
 }
