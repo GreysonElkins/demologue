6574459f239ffb911ff617b0659c5aaa76f25fef
Greyson Elkins <greysonelkins@gmail.com>
2021-12-11 18:32:43 -0700

    Size profile to player

--- client/src/components/Profile/User/index.tsx
+++ client/src/components/Profile/User/index.tsx
@@ -2,8 +2,18 @@ import Header from './Header'
 
 import '../index.scss'
 import User from 'types/User'
+import UserBands from './UserBands'
+import { usePlayer } from 'context/Player'
 
-const UserProfile:React.FC<{user: User}> = ({ user }) => 
-  <div className="Profile"><Header user={user}/></div>
+const UserProfile: React.FC<{ user: User }> = ({ user }) => {
+  const { isMounted } = usePlayer()
+  return (
+  <div className="Profile">
+    <Header user={user} />
+    <section className={`user-profile-body ${isMounted ? 'with-player' : ''}`}>
+      <UserBands user={user} />
+    </section>
+  </div>
+)}
 
 export default UserProfile
