53131b298535a9400ca82a6c5b35071f1f371229
Greyson Elkins <greysonelkins@gmail.com>
2021-12-11 18:32:25 -0700

    Reuse avatars

--- client/src/components/Profile/User/Header.tsx
+++ client/src/components/Profile/User/Header.tsx
@@ -1,27 +1,18 @@
-import { useMemo } from 'react'
 import User from 'types/User'
 import UserImage from './UserImage'
-import BandAvatar from 'style/Icon/BandAvatar'
 
 import './UserProfile.scss'
 
-const Header: React.FC<{ user: User }> = ({ user }) => {
-  const bandIds = useMemo(() => user?.bandIds() || [], [JSON.stringify(user)])
-  const bandIcons = () => bandIds.map(id => <BandAvatar key={`band-avatar-${id}`} bandId={id} hideLabel />)
-  return (
-    <section className="header user">
-      <UserImage user={user} />
-      <div className="info">
-        <div className="top-line">
-          <h3>{user.displayName}</h3>
-        </div>
-        <h4>Bands:</h4>
-        <div className="user-bands">
-          {bandIcons()}
-        </div>
+const Header: React.FC<{ user: User }> = ({ user }) => (
+  <section className="header user">
+    <UserImage user={user} />
+    <div className="info">
+      <div className="top-line">
+        <h3>{user.displayName}</h3>
       </div>
-    </section>
-  )
-}
+    </div>
+  </section>
+)
+
 
 export default Header
