64c55be06651bb19579f1e9486f34e5d2961a3ce
Greyson Elkins <greysonelkins@gmail.com>
2021-12-14 11:58:37 -0700

    Change main with mounted player // move Providers out

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,22 +1,25 @@
-import Providers from 'context'
 import Router from 'views'
 
 import Header from 'components/Header'
 import Sidebar from 'components/Menu/Sidebar'
 import MobileNav from 'components/Menu/MobileNav'
 import './App.scss'
+import { usePlayer } from 'context/Player'
 
-const App: React.FC = () => (
-  <Providers>
-    <Header />
-    <div className="main-wrapper">
-      <Sidebar />
-      <main>
-        <Router />
-      </main>
-    </div>
-    <MobileNav />
-  </Providers>
-)
+const App: React.FC = () => {
+  const { isMounted } = usePlayer()
+  return (
+    <>
+      <Header />
+      <div className="main-wrapper">
+        <Sidebar />
+        <main className={isMounted ?  "with-player" : "" }>
+          <Router />
+        </main>
+      </div>
+      <MobileNav />
+    </>
+  )
+}
 
 export default App
