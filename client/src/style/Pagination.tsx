c1944d193727924edca98ffd45ad534c8a7e4e11
Greyson Elkins <greysonelkins@gmail.com>
2021-12-14 13:12:40 -0700

    Initial commit

---
+++ client/src/style/Pagination.tsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from 'react'
+import _ from 'lodash'
+import { Icon } from './Icon'
+
+type Props = {
+  startPage?: number
+  pageCount: number
+  onChange?: (page: number) => void
+  runOnStartUp?: boolean
+}
+
+const Pagination: React.FC<Props> = ({
+  startPage = 1,
+  pageCount,
+  onChange,
+  runOnStartUp = false,
+}) => {
+  const [page, setPage] = useState<number>(startPage)
+  const [startingUp, setStartingUp] = useState<boolean>(!runOnStartUp)
+  let list: number[] = []
+  let startTrunc = false
+  let endTrunc = false
+
+  useEffect(() => {
+    if (startingUp) return setStartingUp(false)
+    if (!onChange) return
+    onChange(page)
+  }, [page])
+
+  if (!pageCount) return <></>
+
+  const jumpPage = (pageNumber: number) => setPage(pageNumber)
+
+  const nextPage = () => setPage((prev) => (prev < pageCount ? prev + 1 : prev))
+
+  const prevPage = () => setPage((prev) => (prev > 1 ? prev - 1 : prev))
+
+  if (pageCount >= 7) {
+    //allow for truncated pagination
+    // credit Albert De Guzman, @werevamp
+    if (page <= 4) {
+      list = _.range(1, 6)
+      // range is 1 - 5
+      // no initial truncation
+      endTrunc = true
+    } else if (page === 4 && pageCount === 7) {
+      list = _.range(1, 8)
+      // range is 1 - 7
+      // no initial or end truncation
+    } else {
+      // add initial truncation
+      startTrunc = true
+      if (page + 3 === pageCount) {
+        list = _.range(pageCount - 5, pageCount + 1)
+      } else if (page + 3 > pageCount) {
+        list = _.range(pageCount - 4, pageCount + 1)
+      } else {
+        list = _.range(page - 2, page + 3)
+        endTrunc = true
+      }
+    }
+  } else {
+    list = _.range(1, pageCount + 1)
+  }
+  return (
+    <div>
+      <button onClick={prevPage}>
+        <Icon icon="angle-left" /> Prev
+      </button>
+      {startTrunc && (
+        <>
+          <button onClick={() => jumpPage(1)}>{/* currentPage={false} */}1</button>
+          {/* TRUNCATION */}
+          <div className="truncation">&hellip;</div>
+        </>
+      )}
+      {list.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => jumpPage(pageNumber)}
+          //   currentPage={page === pageNumber}
+        >
+          {pageNumber}
+        </button>
+      ))}
+      {endTrunc && (
+        <>
+          <div className="truncation">&hellip;</div>
+          <button onClick={() => jumpPage(pageCount)}>
+            {/* currentPage={false} */}
+            {pageCount}
+          </button>
+        </>
+      )}
+      <button onClick={nextPage}>
+        Next <Icon icon="angle-right" />
+      </button>
+    </div>
+  )
+}
+
+export default Pagination
