71a89f2e5e828c66a4d41108baeb633545ab1a3b
Greyson Elkins <greysonelkins@gmail.com>
2021-12-15 13:46:33 -0700

    Only print bands user is member of

--- client/src/components/Profile/User/UserBands.tsx
+++ client/src/components/Profile/User/UserBands.tsx
@@ -6,17 +6,23 @@ import BandAvatar from 'style/Icon/BandAvatar'
 import './UserProfile.scss'
 import { useViewer } from 'context/Viewer'
 
-const UserBands: React.FC<{user: User}> = ({ user }) => {
+const UserBands: React.FC<{ user: User }> = ({ user }) => {
   const { user: viewer } = useViewer()
 
   const userIsViewer = () => viewer && viewer.uid === user.uid
 
   const bandIds = useMemo(() => user?.bandIds() || [], [JSON.stringify(user)])
   const bandIcons = () =>
-    bandIds.map(id => <BandAvatar key={`band-avatar-${id}`} bandId={id} labelClass="pop-out-label"/>)
+    bandIds.reduce((avatars, id) => {
+      if (user.bands[id] === 'MEMBER')
+        avatars.push(
+          <BandAvatar key={`band-avatar-${id}`} bandId={id} labelClass="pop-out-label" />
+        )
+      return avatars
+    }, [] as JSX.Element[])
   return (
     <div className="UserBands">
-      {userIsViewer() && <AddBand />} 
+      {userIsViewer() && <AddBand />}
       {/* else => invite to band */}
       {bandIcons()}
     </div>
