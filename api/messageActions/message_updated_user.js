8f64e458b1a5c57811ceddaa661ce8d19c0f1032
Greyson Elkins <greysonelkins@gmail.com>
2021-12-15 17:55:26 -0700

    Add request accepted message

--- api/messageActions/message_updated_user.js
+++ api/messageActions/message_updated_user.js
@@ -3,17 +3,25 @@ const knex = require('../knex')
 module.exports = async (payload) => {
   const { role, user_id, band_id } = JSON.parse(payload)
   const user = await knex.select('display_name').from('users').where('uid', user_id)
+  const band = await knex.select('name').from('band').where('id', band_id)
+  let metadata
   switch (role) {
     case 'REQUEST':
-      const metadata = JSON.stringify({ sender: user[0].displayName, senderId: user_id })
+      metadata = JSON.stringify({ sender: user[0].displayName, senderId: user_id })
       await knex('message').insert({ message_type: 'MEMBER_REQUEST', metadata, band_id })
       break
-      case 'INVITE':
-        break
-        case 'MEMBER':
-          const metadata = JSON.stringify({ sender: user[0].displayName, senderId: user_id })
-          await knex('message').insert({ message_type: 'NEW_MEMBER', metadata, band_id })
-      case 'SUPPORT':
-        break
+    case 'INVITE':
+      break
+    case 'MEMBER':
+      metadata = JSON.stringify({ sender: user[0].displayName, senderId: user_id })
+      const confirmInviteMetadata = JSON.stringify({ sender: band[0].name })
+      await knex('message').insert({ message_type: 'NEW_MEMBER', metadata, band_id })
+      await knex('message').insert({
+        message_type: 'REQUEST_ACCEPTED',
+        metadata: confirmInviteMetadata,
+        user_id,
+      })
+    case 'SUPPORT':
+      break
   }
 }
