ea712018e62858925b212e13d72c18d7908683c5
Greyson Elkins <greysonelkins@gmail.com>
2021-12-16 13:01:56 -0700

    Prettier

--- client/src/types/Message/Metadata.d.ts
+++ client/src/types/Message/Metadata.d.ts
@@ -1,21 +1,38 @@
 import { MessageType } from '.'
 
-type BaseMetadata = {
+type gqlMetadata = {
+  [key: string]: any
+}
+
+class BaseMetadata {
   sender: string
-  senderId?: string
+  
+  constructor(gqlMetadata: string | gqlMetadata) {
+    const { senderId, exclude, sender } = typeof gqlMetadata === "string" ? JSON.parse(gqlMetadata) : gqlMetadata
+    this.senderId = senderId || ''
+    this.exclude = exclude || []
+    this.sender = sender || ''
+  }
 }
 
-interface MemberRequest extends BaseMetadata {
+class MemberApproved extends BaseMetadata {
   senderId: string
+  exclude: string[]
+  approved_by_name: string
+  constructor (gqlMetadata: string) {
+    const { approved_by_name, ...meta } = JSON.parse(gqlMetadata)
+    super(meta)
+    this.approved_by_name = approved_by_name
+  }
 }
 
-type Metadata<T extends MessageType> = 
-  T extends 'BAND_UPDATE' | 'REQUEST_ACCEPTED'
-  ? BaseMetadata
-  : T extends 'MEMBER_REQUEST'
-  ? MemberRequest
-  : {
-      error: 'An unidentified MessageType was parsed'
-      sender: 'unknown'
-      senderId: ''
+export const parseMetadata = (data: string, type: MessageType) => {
+  switch (type) {
+    case "MEMBER_APPROVED":
+      return new MemberApproved(data) 
+    case "MEMBER_REQUEST": 
+    case "REQUEST_ACCEPTED":
+    default: 
+        return new BaseMetadata(data)
     }
+}
