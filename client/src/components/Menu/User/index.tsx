e09d45f6bb1a93212e7cf06837e5b175a9b53e14
Greyson Elkins <greysonelkins@gmail.com>
2021-12-16 12:59:02 -0700

    Add unread pill

--- client/src/components/Menu/User/index.tsx
+++ client/src/components/Menu/User/index.tsx
@@ -1,23 +1,38 @@
 import { Link } from 'react-router-dom'
 import { useViewer } from 'context/Viewer'
+import { useMessages } from 'context/Messages'
 
 import UserAvatar from 'style/Icon/UserAvatar'
 import DropdownCta from 'style/button/DropdownCta'
 
 import './UserMenu.scss'
 
+const Trigger: React.FC<{uid: string}> = ({ uid }) => {
+  const { hasUnread } = useMessages()
+  return (
+    <div className={`user-menu-trigger${hasUnread ? ' unread' : ''}`}>
+      <div className="alert-pill" />
+      <UserAvatar userId={uid} hideLabel />
+    </div>
+  )
+}
+
 const UserMenu: React.FC = () => {
   const { logout, user } = useViewer()
+  const { hasUnread } = useMessages()
 
   if (!user) return <></>
 
   return (
-    <DropdownCta altTrigger={<UserAvatar userId={user.uid} hideLabel />}>
+    <DropdownCta altTrigger={<Trigger uid={user.uid} />}>
       <nav className="UserMenu">
         <section className="user-section">
           <Link to="/user">{user?.displayName || 'Edit Profile'}</Link>
         </section>
-        <Link to="/inbox">Notifications</Link>
+        <div className={`user-notifications${hasUnread ? ' unread' : ''}`}>
+          <div className="alert-pill" />
+          <Link to="/inbox">Notifications</Link>
+        </div>
         <Link to="/search-bands">Join Bands</Link>
         <section className="log-out-section">
           <button onClick={logout}>Log out</button>
