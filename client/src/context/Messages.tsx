5a2c9c0da4bb8c2c3a335395401327ea8178872e
Greyson Elkins <greysonelkins@gmail.com>
2021-12-15 18:40:40 -0700

    Parse messages

--- client/src/context/Messages.tsx
+++ client/src/context/Messages.tsx
@@ -1,8 +1,9 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import useViewerBands from 'hooks/useViewerBands'
 import { getMessages } from 'scripts/api/demologue/query/message'
+import { Message, BandMessage } from 'types/Message'
 
-type Inbox = { direct: []; toBand: [] }
+type Inbox = { direct: Message[]; toBand: BandMessage[] }
 
 type MessagesContextValue = {
   loading: boolean
@@ -13,7 +14,7 @@ const MessagesContext = createContext({} as MessagesContextValue)
 
 export const MessagesProvider: React.FC = ({ children }) => {
   const { bandIds, viewer } = useViewerBands()
-  const [ loading, setLoading ] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true)
   const [messages, setMessages] = useState<Inbox>({ direct: [], toBand: [] })
   const { data, isFetching } = getMessages({ bands: bandIds, userId: viewer?.uid || null})
 
@@ -23,7 +24,17 @@ export const MessagesProvider: React.FC = ({ children }) => {
   }, [viewer, isFetching])
 
   useEffect(() => {
-    console.log({ data })
+    if (!data) return
+    const incomingMessages = {
+      direct: data.messages.map(message => new Message(message)),
+      toBand: data.messagesByBands.reduce((messages, message) => {
+        const bandRole = viewer?.bands[message.band.id]
+        if (bandRole === "MEMBER" || bandRole === "SUPPORT") messages.push(new BandMessage(message))
+        // there probably should be some delineation between what types of messages these roles receive
+        return messages
+      }, [] as BandMessage[])
+    }
+    setMessages(incomingMessages)
   }, [data])
 
   return <MessagesContext.Provider value={{ loading, messages }}>{children}</MessagesContext.Provider>
