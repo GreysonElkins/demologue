d12c0bff81a1b41effcf7842314cac0fea39b316
Greyson Elkins <greysonelkins@gmail.com>
2021-12-17 18:21:58 -0700

    Add track triggers

--- api/listenToDb.js
+++ api/listenToDb.js
@@ -1,21 +1,27 @@
 const { getClient } = require('./pg')
 const messageUpdatedUser = require('./messageActions/message_updated_user')
+const messageUpdatedTrack = require('./messageActions/message_updated_track')
 
 module.exports = async () => {
   const client = await getClient()
   const queries = [
-    'band_member_update'
+    'band_member_update',
+    'track_update',
+    'new_track'
   ]
 
   queries.forEach(query => {
     client.query(`LISTEN ${query}`)
   })
 
-  client.on('notification', async ({ payload, channel }) => {
+  client.on('notification', async ({ payload, channel, ...props }) => {
     switch (channel) {
       case ('band_member_update'): 
         messageUpdatedUser(payload)
         break;
+      case ('track_update'):
+      case ('new_track'):
+        messageUpdatedTrack(channel, payload)
       default: console.warn(`${channel} was called but has no actions`)
     }
   })
