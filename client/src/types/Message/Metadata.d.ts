8b9cda8b9b2284646ae71a601c8f327fc6b372dc
Greyson Elkins <greysonelkins@gmail.com>
2021-12-20 12:14:22 -0700

    Add new track interface

--- client/src/types/Message/Metadata.d.ts
+++ client/src/types/Message/Metadata.d.ts
@@ -9,10 +9,20 @@ interface MemberRequest extends BaseMetadata {
   senderId: string
 }
 
+interface NewTrack extends BaseMetadata {
+  exclude: string[]
+  senderId: number
+  trackId: number
+  trackName?: string
+  uploadedBy: string
+}
+
 export type Metadata<T extends MessageType> = T extends 'BAND_UPDATE' | 'REQUEST_ACCEPTED'
   ? BaseMetadata
   : T extends 'MEMBER_REQUEST'
   ? MemberRequest
+  : T extends 'NEW_TRACK' 
+  ? NewTrack 
   : {
       error: 'An unidentified MessageType was parsed'
       sender: 'unknown'
