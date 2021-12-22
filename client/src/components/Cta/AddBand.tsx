d61d19f61b635c90f9ea4949f6b48ba458a8bcfa
Greyson Elkins <greysonelkins@gmail.com>
2021-12-22 14:39:46 -0700

    Pass optional className to CTA

--- client/src/components/Cta/AddBand.tsx
+++ client/src/components/Cta/AddBand.tsx
@@ -3,12 +3,12 @@ import useModal from 'hooks/useModal'
 import Cta from 'style/button/Cta'
 import AddBandModal from 'components/Modal/AddBand'
 
-const AddBand: React.FC = () => {
+const AddBand: React.FC<{className?: string}> = ({ className }) => {
   const { toggle, isOpen, secondOption: skipOptions } = useModal()
   return (
     <>
       <AddBandModal toggle={toggle} isOpen={isOpen} skipOptions={skipOptions} />
-      <Cta cta="Two" collapse onClick={() => toggle(false)} icon="plus">
+      <Cta cta="Two" collapse onClick={() => toggle(false)} icon="plus" className={className}>
         Add a Band
       </Cta>
     </>
